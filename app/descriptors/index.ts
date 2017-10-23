07414fc535e7b0898532d6fbb456a7e0fd3c833f
Adam Kariv <adam.kariv@gmail.com>
2017-10-23 11:14:52 +0300

    Add defaults in dropdown as well (+association and org fixes)

--- app/descriptors/index.ts
+++ app/descriptors/index.ts
@@ -1,6 +1,8 @@
 'use strict';
 
+import descriptorOrgAssociation from './org/association';
 import descriptorOrgCompany from './org/company';
+import descriptorOrg from './org';
 import descriptorBudget2Dig from './budget/budget2dig';
 import descriptorBudget4Dig from './budget/budget4dig';
 import descriptorBudget6Dig from './budget/budget6dig';
@@ -8,7 +10,9 @@ import descriptorBudget8Dig from './budget/budget8dig';
 import {Descriptor} from '../model/descriptor';
 
 let descriptors: Descriptor[] = [
+  descriptorOrgAssociation,
   descriptorOrgCompany,
+  descriptorOrg,
   descriptorBudget2Dig,
   descriptorBudget4Dig,
   descriptorBudget6Dig,
