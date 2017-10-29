3c2b0b0c45745e8bfcb0d8700c772f72fec11f2b
Adam Kariv <adam.kariv@gmail.com>
2017-10-29 17:11:25 +0200

    Budget Func pages

--- app/descriptors/index.ts
+++ app/descriptors/index.ts
@@ -3,6 +3,8 @@
 import descriptorOrgAssociation from './org/association';
 import descriptorOrgCompany from './org/company';
 import descriptorOrg from './org';
+import descriptorBudgetFunc from './budget/budgetFunc';
+import descriptorBudgetFuncDetail from './budget/budgetFuncDetail';
 import descriptorBudget2Dig from './budget/budget2dig';
 import descriptorBudget4Dig from './budget/budget4dig';
 import descriptorBudget6Dig from './budget/budget6dig';
@@ -13,6 +15,8 @@ let descriptors: Descriptor[] = [
   descriptorOrgAssociation,
   descriptorOrgCompany,
   descriptorOrg,
+  descriptorBudgetFunc,
+  descriptorBudgetFuncDetail,
   descriptorBudget2Dig,
   descriptorBudget4Dig,
   descriptorBudget6Dig,
