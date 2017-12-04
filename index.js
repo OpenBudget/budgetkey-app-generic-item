75f8ad1703ef6bd5fa036b3771b04c34d206b7ad
Ori Hoch <ori@uumpa.com>
2017-12-04 17:37:10 +0200

    allow to get themes from /themes folder, makes it easier to use a shared themes volume

--- index.js
+++ index.js
@@ -34,12 +34,21 @@ app.get(basePath + '*', function(req, res) {
   }
 
   var theme = typeof(req.query.theme) !== "undefined" ? req.query.theme : '';
-  var themeFilePath = theme !== '' ? path.resolve(__dirname, 'theme.'+req.query.theme+'.json') : null;
+  var themeFileName = theme !== '' ? 'theme.'+req.query.theme+'.json' : null;
   var themeScript = '';
-  if (themeFilePath && fs.existsSync(themeFilePath)) {
-    var themeJson = JSON.parse(fs.readFileSync(themeFilePath));
-    for (var key in themeJson) {
-      themeScript += key+"="+JSON.stringify(themeJson[key])+";";
+  if (themeFileName) {
+    var themeJson = null;
+    // try the themes root directory first - this allows mount multiple themes in a single shared docker volume
+    if (fs.existsSync(path.resolve('/themes', themeFileName))) {
+      themeJson = JSON.parse(fs.readFileSync(path.resolve('/themes', themeFileName)));
+    // fallback to local file - for local development / testing
+    } else if (fs.existsSync(path.resolve(__dirname, themeFileName))) {
+      themeJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, themeFileName)));
+    }
+    if (themeJson) {
+      for (var key in themeJson) {
+        themeScript += key+"="+JSON.stringify(themeJson[key])+";";
+      }
     }
   }
 
