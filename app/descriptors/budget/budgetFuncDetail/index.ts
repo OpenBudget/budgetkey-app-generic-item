98815ea68c05c97fb9fc482193d5ae6b3e5e0a41
Adam Kariv <adam.kariv@gmail.com>
2018-01-10 18:24:33 +0200

    Budget func pages

--- app/descriptors/budget/budgetFuncDetail/index.ts
+++ app/descriptors/budget/budgetFuncDetail/index.ts
@@ -4,6 +4,6 @@ export default {
   preTitleTemplate: require('./pretitle-template.html'),
   amountTemplate: require('../amount-template.html'),
   subtitleTemplate: require('./subtitle-template.html'),
-  textTemplate: require('./text-template.html'),
+  textTemplate: require('./text-template.html') + require('../text-template.html'),
   questions: require('./questions.json')
 };
