c2b010ed709c3269f7ba31d612a36ec2c5dde889
Adam Kariv <adam.kariv@gmail.com>
2018-01-14 09:33:25 +0200

    Budgetand org  questions

--- app/descriptors/budget/budget6dig/index.ts
+++ app/descriptors/budget/budget6dig/index.ts
@@ -5,5 +5,5 @@ export default {
   amountTemplate: require('../amount-template.html'),
   subtitleTemplate: require('./subtitle-template.html'),
   textTemplate: require('../text-template.html'),
-  questions: require('./questions.json')
+  questions: require('../questions.json')
 };
