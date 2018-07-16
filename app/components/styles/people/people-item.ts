b4e5c53b47abf1e8fda7ffa37a484ee77bc0923a
Oded Sharon <oded.sharon@gmail.com>
2018-07-16 16:20:59 +0300

    People UI fixes:  1) Date Display fixed  2) Add disclaimer regarding data

--- app/components/styles/people/people-item.ts
+++ app/components/styles/people/people-item.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import * as _ from 'lodash';
+import * as moment from 'moment';
+
 import {BaseItemInfoComponent} from '../../base-item-info';
 import {DescriptorBase} from '../../../model';
+
 @Component({
   selector: 'people-item',
   template: require('./people-item.html')
@@ -11,6 +13,9 @@ export class PeopleItemComponent extends BaseItemInfoComponent  {
 
   private descriptor: DescriptorBase;
 
+  format_date(txt: string) {
+    return moment(txt, 'YYYY-MM-DD').format('DD-MM-YYYY');
+  }
   setDescriptor(descriptor: DescriptorBase) {
     this.descriptor = this.store.descriptor;
   }
